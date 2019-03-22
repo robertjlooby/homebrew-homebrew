9d0279a55fd694276487ac8b27e4c6f3250a306b
Rob Looby <robertjlooby@gmail.com>
2019-03-22 07:22:50 -0500

    Bump dhall-to-ssh-config to v0.2.0.0

--- Formula/dhall-to-ssh-config.rb
+++ Formula/dhall-to-ssh-config.rb
@@ -1,16 +1,11 @@
 class DhallToSshConfig < Formula
   desc "Tool for generating an ssh config from Dhall"
   homepage "https://github.com/robertjlooby/dhall-ssh-config"
-  url "https://github.com/robertjlooby/dhall-ssh-config/archive/0.1.0.0.tar.gz"
-  sha256 "6cbf72f80640d56d37e6c1ddc40b9c23082b8628027004e343cb7d62321c1a82"
+  url "https://github.com/robertjlooby/dhall-ssh-config/archive/0.2.0.0.tar.gz"
+  sha256 "c2e6297f8c229df0326a635205b65ca1082a8e3d881870c2b4c0ae5c3d154cdb"
 
   head do
     url "https://github.com/robertjlooby/dhall-ssh-config.git"
-  end
-
-  bottle do
-    root_url "https://github.com/robertjlooby/dhall-ssh-config/releases/download/0.1.0.0"
-    sha256 "f51d6448a0a6580377c3dd06a6c04b30b9a8d31d2ebeb6fbae7f67467beff443" => :mojave
   end
 
   depends_on "haskell-stack" => :build
