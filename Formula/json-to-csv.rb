2403baa417c9b4f5a7488b59cd3d326c4516cfce
Rob Looby <robertjlooby@gmail.com>
2019-08-31 10:06:32 -0500

    Add 1.1.0.0 of json-to-csv

--- Formula/json-to-csv.rb
+++ Formula/json-to-csv.rb
@@ -1,8 +1,8 @@
 class JsonToCsv < Formula
   desc "A tool for converting JSON values to comma-separated values (CSVs)"
   homepage "https://github.com/robertjlooby/json-to-csv"
-  url "https://github.com/robertjlooby/json-to-csv/archive/1.0.0.0.tar.gz"
-  sha256 "547b6c320eec9590470fd67b219309ec3164e25b58112f413bd88db3b2af2869"
+  url "https://github.com/robertjlooby/json-to-csv/archive/1.1.0.0.tar.gz"
+  sha256 "5ef9a9ec9fee994f9e516b0d8829fa60e322250587a689a666c9b2c385a852d7"
 
   head do
     url "https://github.com/robertjlooby/json-to-csv.git"
@@ -10,8 +10,8 @@ class JsonToCsv < Formula
 
   bottle do
     cellar :any_skip_relocation
-    root_url "https://github.com/robertjlooby/json-to-csv/releases/download/1.0.0.0"
-    sha256 "69b83cc8f818f10465009365d11c9fc59b507466b65e80ce4c0549048f5b3fbc" => :mojave
+    root_url "https://github.com/robertjlooby/json-to-csv/releases/download/1.1.0.0"
+    sha256 "8030f1eb9f89118089e8ef7109ac9b6afbcbabb261a1454c6ca86ddf0e41392c" => :mojave
   end
 
   depends_on "haskell-stack" => :build
